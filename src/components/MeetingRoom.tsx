16092762752acb248cdcee59d944f54eeb6e3423
coderekamplayo <coderek.amplayo@gmail.com>
2025-05-09 10:07:42 +0800

    Add all the files

--- src/components/MeetingRoom.tsx
+++ src/components/MeetingRoom.tsx
@@ -1,6 +1,14 @@
-function MeetingRoom() {
+import { useCallStateHooks } from "@stream-io/video-react-sdk";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-  
+function MeetingRoom() {
+  const router = useRouter();
+  const [layout,setLayout] = useState<"grid" | "speaker">('speaker');
+  const [showParticipants,setShowParticipants] = useState(false);
+  const { useCallCallingState } = useCallStateHooks();
+
+
   return <div>MeetingRoom</div>
 }
 export default MeetingRoom;
